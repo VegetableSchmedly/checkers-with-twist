b47e79a88b5e8ac7737b9a5be554e2bc309f043e
VegetableSchmedly <iericdaly@gmail.com>
2023-03-01 17:38:17 -0500

    Added Test skeleton

--- CheckersGame.py
+++ CheckersGame.py
@@ -8,9 +8,11 @@ class OutofTurn(Exception):
     """Raised if a player tries to move when its not their turn."""
     pass
 
+
 class InvalidSquare(Exception):
     """Raised if the player does not own the checker present in the square, or if the square doesnt exist."""
     pass
+
 
 class InvalidPlayer(Exception):
     """Raised if the player name is invalid"""
@@ -24,8 +26,7 @@ class GamePiece:
         self._row = row
         self._column = column
         self._color = color
-        self._piece_type = piece_type                   # Can be 'normal', 'king', or 'triple_king'
-
+        self._piece_type = piece_type  # Can be 'normal', 'king', or 'triple_king'
 
     def promote_piece(self):
         if self._piece_type == 'normal':
@@ -61,6 +62,7 @@ class GamePiece:
         """Gets piece type"""
         return self._piece_type
 
+
 class Player:
     """Represents a player in a checkers game."""
 
@@ -68,7 +70,7 @@ class Player:
         """Initializes data members"""
         self._name = name
         self._pieces = []
-        self._color = color                              # "Black" or "White"
+        self._color = color  # "Black" or "White"
         self._captured_pieces = []
 
     def get_king_count(self):
@@ -76,7 +78,7 @@ class Player:
         king_count = 0
         for piece in self._pieces:
             if piece.get_type() == 'king':
-                king_count+=1
+                king_count += 1
         return king_count
 
     def get_triple_king_count(self):
@@ -84,7 +86,7 @@ class Player:
         triple_king_count = 0
         for piece in self._pieces:
             if piece.get_type() == 'triple_king':
-                triple_king_count+=1
+                triple_king_count += 1
         return triple_king_count
 
     def get_captured_pieces_count(self):
@@ -164,7 +166,8 @@ class Checkers:
         elif current_piece.get_type() == 'king':
             self.make_king_move(current_player, current_piece, starting_square_location, destination_square_location)
         elif current_piece.get_type() == 'triple_king':
-            self.make_triple_king_move(current_player, current_piece, starting_square_location, destination_square_location)
+            self.make_triple_king_move(current_player, current_piece, starting_square_location,
+                                       destination_square_location)
 
     def make_normal_move(self, player, moving_piece, start, destination):
         """Makes the directed move"""
@@ -174,16 +177,25 @@ class Checkers:
         move_column = destination[1]
         moving_piece.set_row(move_row)
         moving_piece.set_column(move_column)
-        if abs(move_row - current_row) > 1:             # If 2 rows move, a jump was executed.
+        if abs(move_row - current_row) > 1:  # If a 2 row move, a jump was executed.
             if move_column > current_column:
-                jump_column = current_column+1
+                jump_column = current_column + 1
             else:
-                jump_column = current_column-1
-            jump_row = current_row + (move_row-current_row)
+                jump_column = current_column - 1
+            if move_row > current_row:
+                jump_row = current_row + 1
+            else:
+                jump_row = current_row - 1
             for piece in self._pieces:
-                if piece.get_location == (jump_row, jump_column):
+                if piece.get_location() == (jump_row, jump_column):
                     self.capture_piece(player, piece)
-                    self.check_if_end_of_turn(moving_piece, start, destination)
+                    if self.check_if_end_of_turn(moving_piece, start, destination) is True:   # Called since we made a jump.
+                        self.promotion_check(moving_piece, player)
+                        self._turn += 1
+        else:
+            self.promotion_check(moving_piece)
+            self._turn += 1
+
 
     def make_king_move(self, player, piece, start, destination):
         """Makes a directed move with the rules of a king."""
@@ -191,6 +203,8 @@ class Checkers:
         current_column = start[1]
         move_row = destination[0]
         move_column = destination[1]
+        # else:
+        #     self._turn += 1
 
     def make_triple_king_move(self, player, piece, start, destination):
         """Makes a directed move with the rules of a triple king."""
@@ -198,6 +212,22 @@ class Checkers:
         current_column = start[1]
         move_row = destination[0]
         move_column = destination[1]
+        # else:
+        #     self._turn += 1
+
+    def promotion_check(self, piece, player):
+        """Check if piece is to be promoted, and promote if applicable."""
+        if piece.get_color() == 'Black':
+            if piece.get_type() == 'normal' and piece.get_row() == 0:
+                piece.promote_piece()
+            elif piece.get_type() == 'king' and piece.get_row() == 7:
+                piece.promote_piece()
+
+        if piece.get_color() == 'White':
+            if piece.get_type() == 'normal' and piece.get_row() == 7:
+                piece.promote_piece()
+            elif piece.get_type() == 'king' and piece.get_row() == 0:
+                piece.promote_piece()
 
     def capture_piece(self, player, piece):
         """Removes the piece from the board and adds it to the player's captured list."""
@@ -205,73 +235,81 @@ class Checkers:
         player.add_captured_piece(piece)
 
     def check_if_end_of_turn(self, moving_piece, starting_square, landing_square):
-        """Checks if this is the mid-point in a multi-jump move. If it is a mid-point, does not increment turn."""
-        color = moving_piece.get_color()
-        mid_jump = False
-        if moving_piece.get_color == 'Black':
-            if landing_square[0] - starting_square[0] > 0:      # Moving down
+        """Checks if this is the mid-point in a multi-jump move. If it is a mid-point, return False."""
+        if moving_piece.get_color() == 'Black':
+            if landing_square[0] - starting_square[0] > 0:  # Moving down
                 potential_right_move = (landing_square[0] + 1, landing_square[1] + 1)
                 potential_left_move = (landing_square[0] + 1, landing_square[1] - 1)
                 for piece in self._pieces:
-                    if piece.get_location() == potential_left_move and piece.get_location() == 'White':
+                    if piece.get_location() == potential_left_move and piece.get_color() == 'White':
                         if potential_left_move[0] < 7 and potential_left_move[1] > 0:
-                            if self.get_checker_details((potential_left_move[0] + 1, potential_left_move[1] - 1)) is None:
-                                return
-                    if piece.get_location() == potential_right_move and piece.get_location() == 'White':
+                            if self.get_checker_details(
+                                    (potential_left_move[0] + 1, potential_left_move[1] - 1)) is None:
+                                return False
+                    if piece.get_location() == potential_right_move and piece.get_color() == 'White':
                         if potential_right_move[0] < 7 and potential_right_move[1] < 7:
-                            if self.get_checker_details((potential_right_move[0] + 1, potential_right_move[1] + 1)) is None:
-                                return
-            if landing_square[0] - starting_square[0] < 0:      # Moving up
+                            if self.get_checker_details(
+                                    (potential_right_move[0] + 1, potential_right_move[1] + 1)) is None:
+                                return False
+
+            if landing_square[0] - starting_square[0] < 0:  # Moving up
                 potential_right_move = (landing_square[0] - 1, landing_square[1] + 1)
                 potential_left_move = (landing_square[0] - 1, landing_square[1] - 1)
                 for piece in self._pieces:
-                    if piece.get_location() == potential_left_move and piece.get_location() == 'White':
+                    if piece.get_location() == potential_left_move and piece.get_color() == 'White':
                         if potential_left_move[0] > 0 and potential_left_move[1] > 0:
-                            if self.get_checker_details((potential_left_move[0] + 1, potential_left_move[1] - 1)) is None:
-                                return
-                    if piece.get_location() == potential_right_move and piece.get_location() == 'White':
+                            if self.get_checker_details(
+                                    (potential_left_move[0] - 1, potential_left_move[1] - 1)) is None:
+                                return False
+                    if piece.get_location() == potential_right_move and piece.get_color() == 'White':
                         if potential_right_move[0] > 0 and potential_right_move[1] < 7:
-                            if self.get_checker_details((potential_right_move[0] + 1, potential_right_move[1] + 1)) is None:
-                                return
+                            if self.get_checker_details(
+                                    (potential_right_move[0] - 1, potential_right_move[1] + 1)) is None:
+                                return False
 
-        elif moving_piece.get_color == 'White':
-            if landing_square[0] - starting_square[0] > 0:      # Moving down
+        elif moving_piece.get_color() == 'White':
+            if landing_square[0] - starting_square[0] > 0:  # Moving down
                 potential_right_move = (landing_square[0] + 1, landing_square[1] + 1)
                 potential_left_move = (landing_square[0] + 1, landing_square[1] - 1)
                 for piece in self._pieces:
-                    if piece.get_location() == potential_left_move and piece.get_location() == 'Black':
+                    if piece.get_location() == potential_left_move and piece.get_color() == 'Black':
                         if potential_left_move[0] < 7 and potential_left_move[1] > 0:
-                            if self.get_checker_details((potential_left_move[0] + 1, potential_left_move[1] - 1)) is None:
-                                return
-                    if piece.get_location() == potential_right_move and piece.get_location() == 'Black':
+                            if self.get_checker_details(
+                                    (potential_left_move[0] + 1, potential_left_move[1] - 1)) is None:
+                                return False
+                    if piece.get_location() == potential_right_move and piece.get_color() == 'Black':
                         if potential_right_move[0] < 7 and potential_right_move[1] < 7:
-                            if self.get_checker_details((potential_right_move[0] + 1, potential_right_move[1] + 1)) is None:
-                                return
-            if landing_square[0] - starting_square[0] < 0:      # Moving up
+                            if self.get_checker_details(
+                                    (potential_right_move[0] + 1, potential_right_move[1] + 1)) is None:
+                                return False
+
+            if landing_square[0] - starting_square[0] < 0:  # Moving up
                 potential_right_move = (landing_square[0] - 1, landing_square[1] + 1)
                 potential_left_move = (landing_square[0] - 1, landing_square[1] - 1)
                 for piece in self._pieces:
-                    if piece.get_location() == potential_left_move and piece.get_location() == 'Black':
+                    if piece.get_location() == potential_left_move and piece.get_color() == 'Black':
                         if potential_left_move[0] > 0 and potential_left_move[1] > 0:
-                            if self.get_checker_details((potential_left_move[0] + 1, potential_left_move[1] - 1)) is None:
-                                return
-                    if piece.get_location() == potential_right_move and piece.get_location() == 'Black':
+                            if self.get_checker_details(
+                                    (potential_left_move[0] + 1, potential_left_move[1] - 1)) is None:
+                                return False
+                    if piece.get_location() == potential_right_move and piece.get_color() == 'Black':
                         if potential_right_move[0] > 0 and potential_right_move[1] < 7:
-                            if self.get_checker_details((potential_right_move[0] + 1, potential_right_move[1] + 1)) is None:
-                                return
-        else:
-            self._turn += 1
-            return
-
-
-    def check_if_blocked_in(self, color):
-        """Check if the color has no available moves left, losing them the game."""
+                            if self.get_checker_details(
+                                    (potential_right_move[0] + 1, potential_right_move[1] + 1)) is None:
+                                return False
+        return True
 
     def valid_move_check(self, player_name, starting_square_location, destination_square_location):
         """Raises appropriate exception, if there is one. Otherwise returns [piece, player]"""
-        if player_name.color == 'Black' and self._turn % 2 == 1:
+        current_player = None
+        for player in self._players:
+            if player.get_name() == player_name:
+                current_player = player
+        if current_player is None:
+            raise InvalidPlayer
+        if current_player.get_color() == 'Black' and self._turn % 2 == 1:
             raise OutofTurn
-        if player_name.color == 'White' and self._turn % 2 == 0:
+        if current_player.get_color() == 'White' and self._turn % 2 == 0:
             raise OutofTurn
 
         if starting_square_location[0] > 7 or starting_square_location[0] < 0:
@@ -283,25 +321,17 @@ class Checkers:
         if destination_square_location[1] > 7 or destination_square_location[1] < 0:
             raise InvalidSquare
 
-        current_player = None
-        for player in self._players:
-            if player.get_name() == player_name:
-                current_player = player
-        if current_player is None:
-            raise InvalidPlayer
-
         current_piece = None
         for piece in self._pieces:
             if piece.get_location() == starting_square_location:
                 current_piece = piece
+        if current_piece is None:
+            raise InvalidSquare
         if current_piece.get_color() != current_player.get_color():
             raise InvalidSquare
 
         else:
             return [current_piece, current_player]
-
-
-
 
     # def check_potential_jumps(self, color):
     #     """Checks to see if there are jumps that need to be made prior to any regular moves. Returns True if there is."""
@@ -348,44 +378,6 @@ class Checkers:
     #         return False
     #
 
-
-
-    def valid_move_check(self, player_name, starting_square_location, destination_square_location):
-        """Raises appropriate exception, if there is one. Otherwise returns [piece, player]"""
-        if player_name.color == 'Black' and self._turn % 2 == 1:
-            raise OutofTurn
-        if player_name.color == 'White' and self._turn % 2 == 0:
-            raise OutofTurn
-
-        if starting_square_location[0] > 7 or starting_square_location[0] < 0:
-            raise InvalidSquare
-        if starting_square_location[1] > 7 or starting_square_location[1] < 0:
-            raise InvalidSquare
-        if destination_square_location[0] > 7 or destination_square_location[0] < 0:
-            raise InvalidSquare
-        if destination_square_location[1] > 7 or destination_square_location[1] < 0:
-            raise InvalidSquare
-
-        current_player = None
-        for player in self._players:
-            if player.get_name() == player_name:
-                current_player = player
-        if current_player is None:
-            raise InvalidPlayer
-
-        current_piece = None
-        for piece in self._pieces:
-            if piece.get_location() == starting_square_location:
-                current_piece = piece
-        if current_piece.get_color() != current_player.get_color():
-            raise InvalidSquare
-
-        else:
-            return [current_piece, current_player]
-
-
-
-
     def get_checker_details(self, square_location):
         """Returns the checker details at the location specified."""
         if square_location[0] > 7 or square_location[0] < 0:
@@ -396,14 +388,14 @@ class Checkers:
         for piece in self._pieces:
             if piece.get_location() == square_location:
                 if piece.get_color() == 'Black':
-                    if piece.get_type() == 'normal':                 # Can be 'normal', 'king', or 'triple_king'
+                    if piece.get_type() == 'normal':  # Can be 'normal', 'king', or 'triple_king'
                         return 'Black'
                     elif piece.get_type() == 'king':
                         return 'Black_king'
                     elif piece.get_type() == 'triple_king':
                         return 'Black_Triple_King'
                 if piece.get_color() == 'White':
-                    if piece.get_type() == 'normal':                 # Can be 'normal', 'king', or 'triple_king'
+                    if piece.get_type() == 'normal':  # Can be 'normal', 'king', or 'triple_king'
                         return 'White'
                     elif piece.get_type() == 'king':
                         return 'White_king'
@@ -424,21 +416,28 @@ class Checkers:
                 else:
                     row_list.append(None)
             print(row_list)
-
-
-
-
-
-
+        print('\n')
 
     def game_winner(self):
         """Returns the name of the player who won the game. Or "Game has not ended." if applicable."""
 
 
-game = Checkers()
-game.print_board()
-game.create_player('Eric', 'Black')
-game.create_player('Maggie', 'White')
-print(game.get_checker_details((0,0)))
-print(game.get_checker_details((0,1)))
 
+
+
+if __name__ == '__main__':
+    game = Checkers()
+    game.print_board()
+    game.create_player('Eric', 'Black')
+    game.create_player('Maggie', 'White')
+    game.play_game('Eric', (5, 0), (4, 1))
+    game.play_game('Maggie', (2, 1), (3,2))
+    game.play_game('Eric', (5, 2), (4, 3))
+    game.play_game('Maggie', (2, 3), (3,4))
+    game.play_game('Eric', (4, 1), (2, 3))
+    game.play_game('Maggie', (3, 4), (4,5))
+    game.play_game('Eric', (5, 6), (4, 7))
+    game.play_game('Maggie', (1, 2), (3,4))
+    game.play_game('Maggie', (3, 4), (5,2))
+    game.play_game('Eric', (6,1), (5,0))
+    game.print_board()
--- CheckersGameTester.py
+++ CheckersGameTester.py
@@ -3,3 +3,74 @@
 # Date:2/24/2023
 # Description:
 
+import CheckersGame as check
+import unittest
+
+class TestGamePiece(unittest.TestCase):
+    """Contains tests for the GamePiece class."""
+
+    def test_1_GamePiece(self):
+        """Test promote_piece"""
+        piece_1 = check.GamePiece(1, 1, 'Black')
+        self.assertEqual(piece_1.get_type(), 'normal')
+        piece_1.promote_piece()
+        self.assertEqual(piece_1.get_type(), 'king')
+        piece_1.promote_piece()
+        self.assertEqual(piece_1.get_type(), 'triple_king')
+
+
+
+
+
+
+class TestPlayer(unittest.TestCase):
+    """Contains tests for the Player class."""
+
+    def test_1_Player(self):
+        """Tests get methods for king, triple_king, and captured pieces counts."""
+
+
+class TestCheckers(unittest.TestCase):
+    """Contains tests for the Checkers class."""
+
+    def test_0_Checkers(self):
+        """Tests all mandatory Exceptions"""
+
+    def test_1_Checkers(self):
+        """Test fill board and get checker details at creation of game."""
+
+    def test_2_Checkers(self):
+        """Tests create player"""
+
+    def test_3_Checkers(self):
+        """Tests play game and normal moves, including a single, double, and triple jump for each color"""
+
+    def test_4_Checkers(self):
+        """Tests promotion check for the ending of a jump, and a regular move. For each color. Checks for captured pieces."""
+
+    def test_5_Checkers(self):
+        """Tests moves and single and double jumps for a king, as well as the promotion to a triple_king"""
+
+    def test_6_Checkers(self):
+        """Tests moves and single and double jumps for a triple_king.
+        Includes jumping over own piece and jumping 2 pieces at once."""
+
+    def test_7_Checkers(self):
+        """Tests winning a game by eliminating all opponent's pieces."""
+
+    def test_8_Checkers(self):
+        """Tests winning by blocking in all of opponent's pieces."""
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
